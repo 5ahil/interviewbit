a28819ab3eb650aa7327ae71449b73ca7e10a88b
5ahil <withshahil@gmail.com>
2018-01-26 16:09:19 +0530

    little modification2

--- leftrightjustification.cpp
+++ leftrightjustification.cpp
@@ -1,7 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<string> fun(vector<string>&a,int k)
+{
+    vector<string>
+
+}
 int main()
 {
-
+    vector<string> a;
+    a.push_back("This");
+    a.push_back("is");
+    a.push_back("an");
+    a.push_back("example");
+    a.push_back("of");
+    a.push_back("text");
+    a.push_back("justification");
+    vector<string> ans=fun(a,16);
+    for(int i=0;i<ans.size();i++)
+    {
+        cout<<ans[i]<<endl;
+    }
+    cout<<"hello world\n";
 
 }
